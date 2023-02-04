9ef28b2d99eb5fabcd4aa29c37c9c013684f8c62
Michal Zielinski <zielinski93.m@gmail.com>
2023-02-04 22:34:19 +0100

    task1.6

--- task1.6.java
+++ task1.6.java
@@ -8,3 +8,20 @@ class Application {
         System.out.println(hash);
     }
 }
+
+
+
+// Zadanie z *
+
+class App {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int c;
+        c = a;
+        a = b;
+        b = c;
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+        }
+    }
